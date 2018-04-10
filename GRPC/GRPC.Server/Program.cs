f5bf95d5456304b1598fe5c73c6066528b8e4af8
suncheng <sunchengsuncheng@gmail.com>
2018-04-10 13:56:53 +0800

    docker not run Console.ReadKey();

--- GRPC/GRPC.Server/Program.cs
+++ GRPC/GRPC.Server/Program.cs
@@ -23,9 +23,8 @@ namespace GRPC.Server
 		    server.Start();
             Console.WriteLine("ThingsToDo server listening on port " + ConfigHelper.Get("Port").ToInt());
 			Console.WriteLine("Press any key to stop the server...");
-			Console.ReadKey();
 
-		    server.ShutdownAsync().Wait();
+		    server.ShutdownTask.Wait();
         }
 	}
 }
