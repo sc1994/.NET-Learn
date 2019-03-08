1e3bab3075ad08c191aae795fdb63c37f03d14cb
孙诚 <sunchengsuncheng@gmail.com>
2019-03-08 18:12:03 +0800

    ef init

---
+++ EFDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDemo
+{
+  /// <summary>
+  /// 文档
+  /// https://docs.microsoft.com/zh-cn/ef/core/
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+
+  class BloggingContext : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySql(@"User ID=root;Password=sun940622;DataBase=Demo;Server=localhost;Port=3306;");
+    }
+  }
+}
